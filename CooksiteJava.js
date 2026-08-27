d7b4755371c632c021f1452ee14df821729282ca
mariano-fdz <m.pena017@ebc.edu.mx>
2026-08-27 12:04:53 -0600

    Add files via upload

---
+++ CooksiteJava.js
@@ -0,0 +1,15 @@
+document.getElementById("botonBusqueda").addEventListener("click", function() {
+    alert("¿Qué receta o producto estás buscando?");
+});
+document.getElementById("botonCampanita").addEventListener("click", function() {
+    alert("No tienes nuevas notificaciones.");
+});
+document.getElementById("botonPerfil").addEventListener("click", function() {
+    alert("Aquí podrás acceder a tu perfil.");
+});
+document.getElementById("botonIniciarSesion").addEventListener("click", function() {
+    alert("Aquí aparecerá el formulario para iniciar sesión.");
+});
+document.getElementById("botonRegistrarse").addEventListener("click", function() {
+    alert("Aquí podrás crear tu cuenta de Cooksite.");
+});
